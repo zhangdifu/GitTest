02f5d27c5d6fd932dbeed170128253b322008f31
zhangfu <zf20151010@163.com>
2019-03-27 13:58:30 +0800

    Initial Commit

---
+++ src/hello/HelloGit.java
@@ -0,0 +1,12 @@
+package hello;
+
+/**
+ * 测试git
+ */
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("123,你好我来自idea");
+        System.out.println("456");
+        System.out.println("789");
+    }
+}
